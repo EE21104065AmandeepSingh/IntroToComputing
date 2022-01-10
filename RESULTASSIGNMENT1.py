d4a637f0fd3a7e7fd5f2d90172c41e640660436c
EE21104065AmandeepSingh <97352314+EE21104065AmandeepSingh@users.noreply.github.com>
2022-01-10 14:33:02 +0530

    assignment 1 python

---
+++ RESULTASSIGNMENT1.py
@@ -0,0 +1,41 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: E:\PEC\PEC-SEM1\computers\Untitled-1.py ===============
+Please enter the first number
+12
+Please enter the second number
+22
+Please enter the third number
+32
+Average of the inputs is: 22.0
+Enter the number of dependents
+1
+Enter the income
+50000
+Enter the gross income
+51000
+Your net income tax is: 10200.0
+Enter your SID
+4065
+Enter your name
+Amandeep
+Enter your gender, Use gender values: 'F', 'M', 'U'(unknown)
+M
+Enter your course name
+EE
+Enter CGPA
+9.8
+[4065, 'Amandeep', 'M', 'EE', 9.8]
+Enter marks of the first student
+99
+Enter marks of the second student
+98
+Enter marks of the third student
+34
+Enter marks of the fourth student
+88
+Enter marks of the fifth student
+77
+[34, 77, 88, 98, 99]
+['Red', 'Green', 'White', 'Yellow']
