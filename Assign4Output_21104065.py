8236091cb1113e2df09363612cfcad7752a20303
EE21104065AmandeepSingh <97352314+EE21104065AmandeepSingh@users.noreply.github.com>
2022-02-27 19:36:09 +0530

    Assignment 4 Python
    SID:21104065

---
+++ Assign4Output_21104065.py
@@ -0,0 +1,86 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/amandeepsingh/Windows/PEC-SEM1/computers/assignments/Assign4_21104065/Assign4_21104065.py
+Question1
+Move disk 1 from rod X to rod Z.
+Move disk 2 from rod X to rod Y.
+Move disk 1 from rod Z to rod Y.
+Move disk 3 from rod X to rod Z.
+Move disk 1 from rod Y to rod X.
+Move disk 2 from rod Y to rod Z.
+Move disk 1 from rod X to rod Z.
+----------------------------------------------------------------------------------------------------
+Question2
+Enter number of rows in Pascal's Triangle:7
+Using recursion
+[1]
+[1, 1]
+[1, 2, 1]
+[1, 3, 3, 1]
+[1, 4, 6, 4, 1]
+[1, 5, 10, 10, 5, 1]
+[1, 6, 15, 20, 15, 6, 1]
+Using iteration
+[1]
+[1, 1]
+[1, 2, 1]
+[1, 3, 3, 1]
+[1, 4, 6, 4, 1]
+[1, 5, 10, 10, 5, 1]
+[1, 6, 15, 20, 15, 6, 1]
+----------------------------------------------------------------------------------------------------
+Question3
+Please enter the first integer:12
+Please enter the second integer:6
+Pt. A
+It's callable
+Pt. B
+All vaules aren't non-zero.
+Pt. C
+Filtered out values which are greater than 4 : [5, 6]
+Pt. D
+Converted Set : {5, 6}
+Pt. E
+Immutable Set : frozenset({5, 6})
+Pt. F
+Maximum value : 6
+Hash value of max value : 6
+----------------------------------------------------------------------------------------------------
+Question4
+I am Amandeep Singh. My SID is 21104065
+Object deleted
+----------------------------------------------------------------------------------------------------
+Question5
+Please enter employee name:
+Mehak
+Please enter employee's salary
+40000
+Do you want to add details of more employees?Please input Y or NY
+Please enter employee name:
+Ashok
+Please enter employee's salary
+50000
+Do you want to add details of more employees?Please input Y or NY
+Please enter employee name:
+Viren
+Please enter employee's salary
+70000
+Do you want to add details of more employees?Please input Y or NN
+{'Mehak': 40000, 'Ashok': 50000, 'Viren': 70000}
+Pt. A
+Please enter employee name:
+Mehak
+Please enter new salary of the employee:70000
+{'Mehak': 70000, 'Ashok': 50000, 'Viren': 70000}
+Pt. B
+Please enter name of employee whose record is to be deleted:Viren
+{'Mehak': 70000, 'Ashok': 50000}
+----------------------------------------------------------------------------------------------------
+Question6
+George's word:ate
+['a', 'e', 't']
+Barbie's new word:tea
+['a', 'e', 't']
+Friendship is real
+-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
